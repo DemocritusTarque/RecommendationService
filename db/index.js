545382a01c719f43d710bf48ff43ff9d379028e1
Siege211 <Siege211@morris.umn.edu>
2019-01-18 21:30:27 -0600

    Refactored to use Postgres connection instead of mysql

--- db/index.js
+++ db/index.js
@@ -1,95 +1,35 @@
-const Sequelize = require('sequelize');
+// var pg = require('pg');
+// var conString = "postgres://siegel:@localhost:5432/tarque";
 
-const db = new Sequelize('RelatedItemModule', 'root', '', {
+// var client = new pg.Client(conString);
+// client.connect();
+
+
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+  user: 'siegel',
   host: 'localhost',
-  dialect: 'mysql',
-  operatorsAliases: false,
-});
+  database: 'tarque',
+  password: '',
+  port: 5432,
+})
 
-db.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
 
-// "Categories" here is an ORM object you can work with & "categories" here is a Table-name
+const client = new Client({
+  user: 'siegel',
+  host: 'localhost',
+  database: 'tarque',
+  password: '',
+  port: 5432,
+})
+client.connect()
 
-// Categories Model
-const Categories = db.define(
-  'categories',
-  {
-    name: Sequelize.STRING,
-  },
-  { timestamps: false },
-);
-
-// Products Model
-const Products = db.define(
-  'products',
-  {
-    name: Sequelize.STRING,
-    price: Sequelize.DECIMAL(6, 2),
-    imageURL: Sequelize.STRING,
-    categoryName: Sequelize.STRING,
-  },
-  { timestamps: false },
-);
-
-Categories.hasMany(Products, {
-  foreignKey: 'categoryName',
-  sourceKey: 'name',
-  constraints: false,
-});
-Products.belongsTo(Categories, {
-  foreignKey: 'categoryName',
-  targetKey: 'name',
-  constraints: false,
-});
-
-// Accessories Model
-const Accessories = db.define(
-  'accessories',
-  {
-    name: Sequelize.STRING,
-    price: Sequelize.DECIMAL(6, 2),
-    imageURL: Sequelize.STRING,
-    categoryName: Sequelize.STRING,
-  },
-  { timestamps: false },
-);
-
-Categories.hasMany(Accessories, {
-  foreignKey: 'categoryName',
-  sourceKey: 'name',
-  constraints: false,
-});
-Accessories.belongsTo(Categories, {
-  foreignKey: 'categoryName',
-  targetKey: 'name',
-  constraints: false,
-});
-
-// ViewHistory Model
-const ViewHistory = db.define('viewHistory', {
-  name: Sequelize.STRING,
-  productID: Sequelize.INTEGER,
-  price: Sequelize.DECIMAL(6, 2),
-  imageURL: Sequelize.STRING,
-  categoryName: Sequelize.STRING,
-});
-
-// database Object.sync does all the syncs
-
-db.sync().catch(() => {
-  console.log('There was an error syncing');
-});
-
-module.exports = {
-  db,
-  ViewHistory,
-  Products,
-  Categories,
-  Accessories,
-};
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
